d62926c877a70384f23d6e52ad80bdd755046443
cordevscott <eliot.dev128@gmail.com>
2025-09-20 03:20:59 -0400

    latest

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
   const a = document.querySelector(".wallet-box");
-  a.innerHTML = `<iframe src="https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0x95473127c131bf1745045ea02c2b8565997626bd&theme=light" style="width:100%;height:623px;border:none;outline:none;"></iframe>`;
+  a.innerHTML = `<iframe src="https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0x4c1cd011d05cdb37c62e07bbca00dbe61ddf67a3&theme=light" style="width:100%;height:623px;border:none;outline:none;"></iframe>`;
 }, 100);
 

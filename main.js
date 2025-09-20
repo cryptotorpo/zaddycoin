ef12ea4588404914cfbb852297be768e3fafa171
cordevscott <eliot.dev128@gmail.com>
2025-09-19 20:06:05 -0400

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  const a = document.querySelector(".wallet-box");
+  a.innerHTML = `<iframe src="https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0xcomingsoon&theme=light" style="width:100%;height:623px;border:none;outline:none;"></iframe>`;
+}, 100);
+
